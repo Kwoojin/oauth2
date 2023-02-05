60294cf5206b980c5eaae9fbddf7426b61120a6d
Kwoojin <dnwls488@naver.com>
2023-02-05 23:35:20 +0900

    OAuth2, OIDC 관련 테스트

--- src/main/java/io/security/oauth2/IndexController.java
+++ src/main/java/io/security/oauth2/IndexController.java
@@ -1,13 +1,78 @@
 package io.security.oauth2;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping("/")
     public String index() {
+
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        System.out.println("clientRegistration.getClientId() = " + clientRegistration.getClientId());
+        System.out.println("clientRegistration.getRedirectUri() = " + clientRegistration.getRedirectUri());
+
         return "index";
+    }
+
+
+    @GetMapping("/user")
+    public OAuth2User user(String accessToken) {
+
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX);
+
+        OAuth2UserRequest oAuth2UserRequest = new OAuth2UserRequest(clientRegistration, oAuth2AccessToken);
+
+
+        DefaultOAuth2UserService defaultOAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = defaultOAuth2UserService.loadUser(oAuth2UserRequest);
+
+        return oAuth2User;
+    }
+
+    @GetMapping("/oidc")
+    public OAuth2User oidc(String accessToken, String idToken) {
+
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        OAuth2AccessToken oAuth2AccessToken = new OAuth2AccessToken(OAuth2AccessToken.TokenType.BEARER, accessToken, Instant.now(), Instant.MAX);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put(IdTokenClaimNames.ISS, "http://localhost:8080/realms/oauth2");
+        claims.put(IdTokenClaimNames.SUB, "OIDC0");
+        claims.put("preferred_username", "user");
+
+        OidcIdToken oidcIdToken = new OidcIdToken(idToken, Instant.now(), Instant.MAX, claims);
+
+        OidcUserRequest oAuth2UserRequest = new OidcUserRequest(clientRegistration, oAuth2AccessToken, oidcIdToken);
+        OidcUserService oidcUserService = new OidcUserService();
+
+        OidcUser oidcUser = oidcUserService.loadUser(oAuth2UserRequest);
+
+        return oidcUser;
     }
 }
--- src/main/java/io/security/oauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/SecurityConfig.java
@@ -1,20 +1,44 @@
 package io.security.oauth2;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests().anyRequest().authenticated();
-//        http.formLogin();
-//        http.apply(new CustomSecurityConfigurer().setFlag(false));
-//        return http.build();
-//    }
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(authRequest -> authRequest.anyRequest().permitAll()
+                )
+                .oauth2Login(withDefaults()
+//                        .loginPage("/loginPage")
+
+                )
+                .logout(logout -> logout
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                )
+                .build();
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
+        return successHandler;
+    }
 
 
 }
