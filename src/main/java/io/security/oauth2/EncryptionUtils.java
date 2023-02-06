41f5362dfddbe3493f8e4806d11f9b480913ef6a
Kwoojin <dnwls488@naver.com>
2023-02-06 23:59:45 +0900

    여러가지 Utils 추가

---
+++ src/main/java/io/security/oauth2/EncryptionUtils.java
@@ -0,0 +1,44 @@
+package io.security.oauth2;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class EncryptionUtils {
+    private static final String alg = "AES/CBC/PKCS5Padding";
+    private static final String key = "majestysecretketf8mnclapq03mvld2"; // 32byte
+    private static final String iv = "majesty3mvlkd02l"; // 16byte
+
+    // 암호화 encryptAES256
+    public static String encrypt(String text) {
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] encrypted = cipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    // 복호화 decryptAES256
+    public static String decrypt(String cipherText) {
+        try {
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+            byte[] decrypted = cipher.doFinal(decodedBytes);
+            return new String(decrypted, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+}
