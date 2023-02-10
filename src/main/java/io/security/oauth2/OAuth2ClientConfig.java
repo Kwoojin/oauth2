bfabdcb245efabba0efc4d629e477c5bf8c31af4
Kwoojin <dnwls488@naver.com>
2023-02-10 23:50:29 +0900

    ClientRegistration Java Bean 테스트

--- src/main/java/io/security/oauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/OAuth2ClientConfig.java
@@ -10,19 +10,19 @@ import org.springframework.security.oauth2.client.registration.InMemoryClientReg
 @Configuration
 public class OAuth2ClientConfig {
 
-//    @Bean
-//    public ClientRegistrationRepository clientRegistrationRepository() {
-//        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-//    }
-//
-//    private ClientRegistration keycloakClientRegistration() {
-//        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
-//                .registrationId("keycloak")
-//                .clientId("oauth2-client-app")
-//                .clientSecret("Bi7RyJ72QB2BNKWS51RC7LhoNDFSLkFQ")
-//                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
-//                .build();
-//    }
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+    }
+
+    private ClientRegistration keycloakClientRegistration() {
+        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
+                .registrationId("keycloak")
+                .clientId("oauth2-client-app")
+                .clientSecret("Bi7RyJ72QB2BNKWS51RC7LhoNDFSLkFQ")
+                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
+                .build();
+    }
 
 
 }
